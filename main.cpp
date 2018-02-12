8d817c28409048bfacbf65be580dadc69eb3887a
root <root@PI3014.depinfo.touchard.edu>
2018-02-12 14:33:48 +0100

    add methode orientation

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 /**********************************************************************
 Author : Philippe SIMIER
-Date   : 30/06/2016
+Date   : 12/02/2018
 Sujet  : Programme démo  la class SenseHat
 Prérequis : copier libSenseHat.a  dans /usr/lib/
           : et     SensHat.h      dans /usr/include/
@@ -22,28 +22,33 @@ float pression;
 float temperature;
 float humidite;
 float x,y,z;
+float pitch,roll,yaw;
 
 
-carte.Effacer();
-sleep(1);
-for (i=0;i<8;i++){
-   carte.AllumerPixel(1,i,BLEU);
-   carte.AllumerPixel(0,i,ROUGE);
-   carte.AllumerPixel(2,i,VERT);
-   sleep(1);
-}
-pression = carte.ObtenirPression();
-std::cout << "pression : " << pression << " hPa"<< std::endl;
+    carte.Effacer();
 
-temperature = carte.ObtenirTemperature();
-std::cout << "Température : " << temperature << " °C" << std::endl;
+    for (i=0;i<8;i++){
+   	carte.AllumerPixel(1,i,BLEU);
+   	carte.AllumerPixel(0,i,ROUGE);
+   	carte.AllumerPixel(2,i,VERT);
+   	sleep(1);
+    }
+    while(1){
+    	pression = carte.ObtenirPression();
+    	std::cout << "pression : " << pression << " hPa"<< std::endl;
 
-humidite = carte.ObtenirHumidite();
-std::cout << "Humidité : " << humidite << " %" << std::endl;
+	temperature = carte.ObtenirTemperature();
+	std::cout << "Température : " << temperature << " °C" << std::endl;
 
-carte.InitialiserAcceleration();
-carte.ObtenirAcceleration(x,y,z);
-std::cout << "accélération x : " << x << " y : " << y << " z : " << z << std::endl;
+	humidite = carte.ObtenirHumidite();
+	std::cout << "Humidité : " << humidite << " %" << std::endl;
 
+	carte.ObtenirAcceleration(x,y,z);
+	std::cout << "accélération x : " << x << "(g) y : " << y << "(g) z : " << z << "(g)" << std::endl;
 
+	carte.ObtenirOrientation(pitch,roll,yaw);
+	std::cout << "orientation pitch : " << pitch << " roll : " << roll << " yaw : " << yaw << std::endl;
+    	sleep(1);
+	system("clear"); 
+    }
 }
