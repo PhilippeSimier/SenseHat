a48467fb2c4326ba8cddfbefa77d8d0210afa4f4
philippeSimier <philaure@wanadoo.fr>
2018-03-01 11:57:32 +0100

    add compass

--- Exemples/compass.cpp
+++ Exemples/compass.cpp
@@ -1,14 +1,20 @@
 /**********************************************************************
     \file   : compass.cpp
     \author : Philippe SIMIER (Touchard Washington Le Mans)
-    \date   : 12/02/2018
+    \date   : 01/03/2018
     \brief  : Programme démonstration de la class SenseHat
+    \details: Affiche les coordonnées sphérique du vecteur champ magnétique
+	      si teta = 0° alors l'axe X est aligné avec la direction du Nord
+	      si teta = -90°   l'axe X est aligné avec l'Est
+	      si teta = +90°   l'axe X est aligné avec l'Ouest
+              si teta = +180 ou -180 l'axe X est aligné avec le Sud
+
     \Compilation : g++ compass.cpp -l SenseHat -l RTIMULib -o compass
 ***********************************************************************/
 
 #include <SenseHat.h>
 #include <iostream>
-#include <math.h>    /* atan2 */
+
 
 #define PI 3.14159265
 
@@ -22,13 +28,10 @@ int main(){
     while(1){
 	carte.ObtenirMagnetismeSpherique(ro, teta, delta);
         system("clear");
-    	std::cout << "θ : " << teta << " °"<< std::endl;
-	std::cout << "δ : " << delta << " °"<< std::endl;
-	std::cout << "ρ : " << ro << " µT"<< std::endl;
-	x = ro * cos(delta) * cos (teta);
-        y = ro * cos(delta) * sin (teta);
-        z = ro * sin(delta);
-        std::cout << "conv x : " << x << "(µT) y : " << y << "(µT) z : " << z << "(µT)" << std::endl;
+    	std::cout << "θ : " << teta  << " °" << std::endl;
+	std::cout << "δ : " << delta << " °" << std::endl;
+	std::cout << "ρ : " << ro    << " µT"<< std::endl;
+
     	usleep(500*1000);
     }
     return 0;
